54d91188cf4f99f059a1aec4af24fc6ae8cc0274
Marco Gallardo <marco.gallardo@tangosource.com>
2013-10-18 16:27:41 -0500

    added events to play and stop recorder; also improved code

--- lib/record_me/helpers.rb
+++ lib/record_me/helpers.rb
@@ -1,15 +1,27 @@
 module RecordMe
   module Helpers
 
-    def record_tag
-      content_tag :div, class: 'location_recorder' do
+    def record_me_location
+      content_tag :div, id: 'recorder_location' do
       end
     end
     
-    def record_script(audio_path = '')
+    def record_me_start(klass)
+      content_tag :div, id: 'recorder_start', class: klass do
+      end
+    end
+    
+    def record_me_stop(klass)
+      content_tag :div, id: 'recorder_stop', class: klass do
+      end
+    end
+    
+    def record_script(audio_path = '', time_lapse = 30)
       javascript_tag do
-        ("var locationRecorder = $('#location_recorder')[0];\n\
-        $.jRecorder(\n\
+        ("var recorderLocation = $('#recorder_location')[0];\n\
+          var recorderStart = $('#recorder_start')[0];\n\
+          var recorderStop = $('#recorder_stop')[0];\n\
+          $.jRecorder(\n\
           {\n\
             host:\"#{audio_path}?filename=audio.wav\",\n\
             callback_started_recording: function(){callback_started(); },\n\
@@ -18,7 +30,15 @@ module RecordMe
             callback_activityTime: function(time){callback_activityTime(time);},\n\
             callback_finished_sending: function(time){ callback_finished_sending() },\n\
             swf_path : \"/assets/jRecorder.swf\",\n\
-          }, $(locationRecorder));\n\
+          }, $(recorderLocation));\n\
+
+          $(recorderStart).click(function(){\n
+            $.jRecorder.record(#{time_lapse});\n
+          });\n
+
+          $(recorderStop).click(function(){\n
+            $.jRecorder.stop();\n
+          });\n
         ").html_safe
       end
     end
