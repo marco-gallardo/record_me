b96ee02fbc9f81dbd5d6ca9653732bdc480a46c4
Marco Gallardo <marco.gallardo@tangosource.com>
2013-12-03 15:25:40 -0600

    saving record in directory specified in record_me.yml

--- lib/record_me/rec.rb
+++ lib/record_me/rec.rb
@@ -2,7 +2,7 @@ module RecordMe
   module Rec
     def record_me_save(filename = params[:filename])
       audio = request.raw_post
-      File.open(filename, 'w+b'){|file| file.puts audio}
+      File.open(RecordMe.configuration["destination"] + "/" + filename, 'w+b'){|file| file.puts audio}
       File.exist?(filename) ? true : false  
     end 
   end
