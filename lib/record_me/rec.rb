3ae6b91f26766b44437063ab5824be81a83b4121
Marco Gallardo <marco.gallardo@tangosource.com>
2013-12-03 11:52:27 -0600

    renamed recording method for controllers; also returning true or false depending if document was created or not

--- lib/record_me/rec.rb
+++ lib/record_me/rec.rb
@@ -1,8 +1,9 @@
 module RecordMe
   module Rec
-    def record(filename = params[:filename])
+    def record_me_save(filename = params[:filename])
       audio = request.raw_post
       File.open(filename, 'w+b'){|file| file.puts audio}
+      File.exist?(filename) ? true : false  
     end 
   end
 end
