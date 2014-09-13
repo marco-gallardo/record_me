5cf1eb5a2a0c061bdc1880b9c4c80575884d68c1
Marco Gallardo <marco.gallardo@tangosource.com>
2014-09-13 13:07:50 -0500

    removing unecessary variables and refactoring line to load YAML config file

--- config/initializer/record_me.rb
+++ config/initializer/record_me.rb
@@ -3,22 +3,11 @@ module RecordMe
   # the Authorization configuration file path
   @file = Dir.pwd + '/config/record_me.yml' 
 
-  # Configuration defaults
-  @config = {
-              "top" => '40%',
-              "left" => '40%',
-              "time" => 30,
-              "name" => 'record.wav',
-              "destination" => 'public/records'
-            }
-
-  @valid_config_keys = @config.keys
-
   # Configure
   def self.configure
     if @file
       # interpret it using YAML using the current environment to extract the configuration
-      @config = YAML.load( ERB.new( @file.read ).result )[ Rails.env ] 
+      @config = YAML.load( File.read(@file) )[ Rails.env ] 
     end 
 
     # creates directory where all records will be saved
