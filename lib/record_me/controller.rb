1616e13c21dd5fc551a3f3d4d8c5bfbf4d5fa958
Marco Gallardo <marco.gallardo@tangosource.com>
2014-09-13 13:15:40 -0500

    improving record_me_save controller method

--- lib/record_me/controller.rb
+++ lib/record_me/controller.rb
@@ -1,10 +1,20 @@
 module RecordMe
   module Controller
     def record_me_save(filename = params[:filename])
-      audio = request.raw_post
-      File.open(RecordMe.configuration["destination"] + "/" + filename, 'w+b'){|file| file.puts audio}
-      File.exist?(filename) ? true : false  
+      audio           = request.raw_post
+      file_with_path = RecordMe.configuration["destination"] + "/" + filename
+
+      return save_file(file_with_path, audio)
     end 
+
+    private
+
+    def save_file(file_with_path, audio)
+      File.open(file_with_path, 'w+b'){|file| file.puts audio}
+
+      File.exist?(file_with_path) ? true : false  
+    end
+
   end
 end
 
